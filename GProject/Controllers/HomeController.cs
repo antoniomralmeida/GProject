16ca0f42e9c4b61b42120cbcddd9e1d0178d9f9c
nilson Xavier <nilsonxavier12@gmail.com>
2017-06-30 00:13:17 -0300

    add View de Usuario, Admin e Gerente

--- GProject/Controllers/HomeController.cs
+++ GProject/Controllers/HomeController.cs
@@ -12,9 +12,70 @@ namespace GProject.Controllers
         {
             return View();
         }
+
+            // View Gerente Admin Sistema
         public ActionResult CadGerente()
         {
             return View();
         }
+            // Fim Gerente Admin Sistema
+
+            // View Gerente de Projeto
+            // view Criar projeto
+        public ActionResult CreateProject()
+        {
+            return View();
+        }
+            // View Atribuir atividades
+        public ActionResult AssignActivities()
+        {
+            return View();
+        }
+            // View Gerenciar Atividades
+        public ActionResult ManageActivities()
+        {
+            return View();
+        }
+            // View	Selecionar membro para projeto
+        public ActionResult SelectMemberProject()
+        {
+            return View();
+        }
+            // View Aprovar finalização de atividades
+        public ActionResult ApproveFinalizationActivities()
+        {
+            return View();
+        }
+            // view Acompanhar atividades
+        public ActionResult TrackActivities()
+        {
+            return View();
+        }
+            // view Visualizar projeto
+        public ActionResult ViewProject()
+        {
+            return View();
+        }
+            // view Finalizar atividade
+        public ActionResult EndActivity()
+        {
+            return View();
+        }
+            // view  Informa duração da atividade
+        public ActionResult ReportsDurationActivity()
+        {
+            return View();
+        }
+        // view Executar atividades
+        public ActionResult PerformActivities()
+        {
+            return View();
+        }
+        // view Informa impedimento de atividade
+        public ActionResult ReportsActivityImpediment()
+        {
+            return View();
+        }
+
     }
 }
